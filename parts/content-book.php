c90646b21d7defba514ceaf0cea2ca564e3f763e
Valentin V <valentinvanh@gmail.com>
2021-11-27 13:47:46 +0100

    Ajout du champ référence dans le template

--- parts/content-book.php
+++ parts/content-book.php
@@ -5,6 +5,7 @@
     </div>
     <div class="book-card__text">
         <p class="book-card__text__titre"><?php the_title(); ?></p>
+        <p class="book-card__text__reference">Réf : Référence</p>
         <p class="book-card__text__resume">Résumé</p>
         <a class="book-card__text__lien" href="<?php the_permalink(); ?>">En savoir plus</a>
     </div>  
